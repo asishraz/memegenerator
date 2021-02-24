c3e3ef57d655684ccce9732dc1428a91d0c85461
asishraz <iamasish3001@gmail.com>
2021-02-24 14:09:41 +0530

    final code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import MemeApp from "./components/MemeApp";
 import "./App.css";
 
-const element = <h1> Meme Generator</h1>;
+// const element = <h1> Meme Generator</h1>;
 
 ReactDOM.render(<MemeApp />, document.getElementById("root"));
